162387f20711f1ed6cb204e0199d1586b569eba2
Ali Khalili <alikhll@outlook.com>
2022-09-14 22:13:45 +0200

    fixed depricated code

--- Actor.Client/Program.cs
+++ Actor.Client/Program.cs
@@ -69,7 +69,7 @@ namespace Actor.Client
                 {
                     optionsBuilder.Configure(options =>
                     {
-                        options.ConnectionString = "AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;DefaultEndpointsProtocol=http;BlobEndpoint=http://127.0.0.1:10000/devstoreaccount1;QueueEndpoint=http://127.0.0.1:8888/devstoreaccount1;TableEndpoint=http://127.0.0.1:10002/devstoreaccount1;";
+                        options.ConfigureQueueServiceClient("AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;DefaultEndpointsProtocol=http;BlobEndpoint=http://127.0.0.1:10000/devstoreaccount1;QueueEndpoint=http://127.0.0.1:8888/devstoreaccount1;TableEndpoint=http://127.0.0.1:10002/devstoreaccount1;");
                         options.QueueNames = new List<string> {
                             "orleansbasic1-azurequeueprovider-0" };
                     });
